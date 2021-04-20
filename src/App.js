c1fb07c77e0d2118457815fc52b073d56be46cfe
zlajo-outfit <58464865+zlajo-outfit@users.noreply.github.com>
2021-04-20 14:30:05 +0200

    Initial Commit - [added main structure, added layout as wrapper element, map through props]

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+import Layout from "./components/ui/Layout";
+
+function App() {
+  return (
+      <Layout>
+      <Switch>
+        <Route path="/" exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+      </Layout>
+  );
+}
+
+export default App;
