6193d671eef043454d3c2dfa70427ec2229a73fb
zlajo-outfit <58464865+zlajo-outfit@users.noreply.github.com>
2021-04-20 15:55:39 +0200

    [Added new meetup page - with new meetup form]

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,11 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 function NewMeetupPage() {
     return (
-        <div>New Meetup page</div>
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm />
+        </section>
     )
 }
 
